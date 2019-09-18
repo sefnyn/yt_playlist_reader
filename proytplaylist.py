8d172f851d709382df8f2e917585e2765a43c6dc
ns <ns@bach.dur.ac.uk>
2019-09-18 12:06:55 +0100

    print number of works

--- proytplaylist.py
+++ proytplaylist.py
@@ -1,14 +1,14 @@
-# Script to convert YT playlist to list of works
+# Script to convert YT playlist to list of works in simple TXT format
 #
 # 1. Download (private) playlist from YT
 # 2. run script:
 #       python3 proyt.py SelectionsYouTube.html
 #
 #
-# Output file contains list of works saved in YT playlist
+# Output file contains list of works in TXT format
 
 import fileinput
-import html
+import html #to convert escaped characters to something more readable
 import re
 VIDEO = 'span id="video-title"'
 selections = []
@@ -17,7 +17,7 @@ print('Starting to process input file...')
 for line in fileinput.input():
      if re.search(VIDEO, line):
          selections.append(line)
-print('Created list of Selections')
+print('Created list of Selections containing ' + str(len(selections)) + ' works')
 
 fh = open('YouTube.txt', 'w')
 for line in selections:
